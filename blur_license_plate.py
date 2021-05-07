7f2c053df9554869d9ed5e0af5f21d474c533822
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2021-05-07 12:00:49 +0900

    draw red boxes arrow LP

--- blur_license_plate.py
+++ blur_license_plate.py
@@ -15,9 +15,27 @@ import numpy as np
 def adjust_pts(pts,lroi):
 	return pts*lroi.wh().reshape((2,1)) + lroi.tl().reshape((2,1))
 
+def blurring(image, factor=3.0):
+    (h,w) = image.shape[:2]
+    kW = int(w / factor)
+    kH = int(h /factor)
+    if kW % 2 == 0:
+        kW -= 1
+    if kH % 2 == 0:
+        kH -= 1
+    return cv2.GaussianBlur(image, (kW, kH), 0)
+
+def draw_losangle(I,pts,color=(1.,1.,1.),thickness=1):
+	assert(pts.shape[0] == 2 and pts.shape[1] == 4)
+
+	for i in range(4):
+		pt1 = tuple(pts[:,i].astype(int).tolist())
+		pt2 = tuple(pts[:,(i+1)%4].astype(int).tolist())
+		cv2.line(I,pt1,pt2,color,thickness)
+
 # input_dir  = sys.argv[1]
 def main(input_dir, net_path):
-    input_dir = "/media/data1/datasets/CJ/s_geq_1_d_leq_20/0000_03/"
+    # input_dir = "/media/data1/datasets/CJ/s_geq_1_d_leq_20/0000_03/"
     output_dir = "./output"
 
     lp_threshold = .5
@@ -33,7 +51,7 @@ def main(input_dir, net_path):
     imgs_paths = sorted(glob(os.path.join(input_dir, "img1", "*.jpg")))
 
     print 'Searching for license plates using WPOD-NET'
-
+    
     for i,img_path in enumerate(imgs_paths):
         print '\t Processing %s' % img_path
         bname = splitext(basename(img_path))[0]
@@ -41,6 +59,16 @@ def main(input_dir, net_path):
         current_frame = gt_file[gt_file[:,0] == frame_id] 
         # print(current_frame)
         ivehicle = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+        def draw_losangle(pts,color=(0,0,255),thickness=1):
+            assert(pts.shape[0] == 2 and pts.shape[1] == 4)
+            for i in range(4):
+                pt1 = tuple(pts[:,i].astype(int).tolist())
+                pt2 = tuple(pts[:,(i+1)%4].astype(int).tolist())
+                cv2.line(ivehicle,pt1,pt2,color,thickness)
+
+        im_wh = np.array(ivehicle.shape[1::-1], dtype=float)
+        is_lp_found = False
         for a in current_frame:
             br = a[2:4] + a[4:6]
             veh_img = ivehicle[a[3]:br[1],a[2]:br[0]]
@@ -53,12 +81,20 @@ def main(input_dir, net_path):
             Llp,LlpImgs,_ = detect_lp(wpod_net,im2single(veh_img),bound_dim,2**4,(240,80),lp_threshold)
 
             if len(LlpImgs):
+                wh_ratio = a[4:6] / im_wh
+                tl_ratio = a[2:4] / im_wh
             	Ilp = LlpImgs[0]
             	Ilp = cv2.cvtColor(Ilp, cv2.COLOR_BGR2GRAY)
             	Ilp = cv2.cvtColor(Ilp, cv2.COLOR_GRAY2BGR)
                 s = Shape(Llp[0].pts)
-                cv2.imwrite('%s/%s_lp.png' % (output_dir,bname),Ilp*255.)
-                writeShapes('%s/%s_lp.txt' % (output_dir,bname),[s])
+                img_pts = Llp[0].pts*wh_ratio.reshape(2,1) + tl_ratio.reshape(2,1)
+                abs_pts = img_pts*im_wh.reshape(2,1)
+                draw_losangle(abs_pts)
+                is_lp_found = True
+        
+        if is_lp_found:
+            cv2.imwrite('%s/%s_lp.png' % (output_dir,bname),ivehicle)
+                # writeShapes('%s/%s_lp.txt' % (output_dir,bname),[s])
             # print current_frame.head()
             # 
             # Ivehicle = cv2.imread(img_path)
